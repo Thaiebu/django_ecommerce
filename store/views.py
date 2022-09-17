edcca5fd16f95f7f5b6b40a607b289d9241c7e8e
Thaiebu <thaiebu785@gmail.com>
2022-09-17 19:46:05 +0530

    MVT created

--- store/views.py
+++ store/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import *
+
+def store(request):
+    products = Product.objects.all()
+    context = {
+        "products":products
+    }
+
+    return render(request,'store/store.html',context)
+
+def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        #get_or_create is used to get the data if not create it
+        order,created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+        # print(items)
+    else:
+        items=[]
+        order = {'get_cart_total':0,'get_cart_item':0}
+    context = {
+        "items":items,
+        "order":order
+    }
+    return render(request,'store/cart.html',context)
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        #get_or_create is used to get the data if not create it
+        order,created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+        # print(items)
+    else:
+        items=[]
+        order = {'get_cart_total':0,'get_cart_item':0}
+    context = {
+        "items":items,
+        "order":order
+    }
+    return render(request,'store/checkout.html',context)
+
+
+
+
